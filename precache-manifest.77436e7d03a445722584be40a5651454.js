5783a1fa913a036ad48f3d57d58b03fe135d60a6
Rajani Sharma <rantasukriti@gmail.com>
2019-07-31 20:15:43 +0530

    Updates

---
+++ precache-manifest.77436e7d03a445722584be40a5651454.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f9bc11ce495f65ee02d211d1cc983a",
+    "url": "/Season_Display/index.html"
+  },
+  {
+    "revision": "9f2cf4d535fdfdb577a0",
+    "url": "/Season_Display/static/css/main.30d62052.chunk.css"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/Season_Display/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "9f2cf4d535fdfdb577a0",
+    "url": "/Season_Display/static/js/main.5ff76d12.chunk.js"
+  },
+  {
+    "revision": "6d060485a6215cf3ac15",
+    "url": "/Season_Display/static/js/runtime~main.214e4280.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Season_Display/precache-manifest.77436e7d03a445722584be40a5651454.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Season_Display/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
